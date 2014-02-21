de1ca6723fa3052fc35dc6d4f3e4c3cc17e00d1e
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-02-21 10:59:21 -0600

    remove junk

--- stepped_care_reboot/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-SteppedCareReboot::Application.config.session_store :cookie_store, key: '_stepped_care_reboot_session'
--- stepped_care_reboot/test/helpers/do_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class DoHelperTest < ActionView::TestCase
-end
