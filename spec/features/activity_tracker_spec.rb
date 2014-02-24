dab2211810172ccd8531c553a04d9e0d4ced9fcc
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-02-24 15:07:20 -0600

    fixed activity tracker slideshow feature test

--- spec/features/activity_tracker_spec.rb
+++ spec/features/activity_tracker_spec.rb
@@ -6,6 +6,7 @@ describe 'activity tracker' do
   describe 'with a slideshow' do
     it 'should allow slideshow navigation' do
       sign_in_participant participants(:participant1)
+      visit '/activity_tracker'
       find('.next-button').click
 
       expect(page).to have_text(slides(:slide2).body)
