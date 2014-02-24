9005c0c19b86ebc36783d6202f52455f83bd3b1b
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-02-24 16:45:10 -0600

    refactoring

--- app/models/content_module.rb
+++ app/models/content_module.rb
@@ -1,22 +1,14 @@
 class ContentModule < ActiveRecord::Base
-  has_many :providers, class_name: 'ContentProvider', dependent: :destroy
+  has_many :providers, -> { order 'position' }, class_name: 'ContentProvider', dependent: :destroy
 
   validates :title, :context, :position, presence: true
   validates_numericality_of :position, greater_than_or_equal_to: 1
 
-  def provider_position=(provider_position)
-    @current_provider_position = provider_position
+  def provider(position)
+    providers.where(position: position).first || ContentProviders::Null.new
   end
 
-  def current_content_provider
-    providers[current_provider_position] || ContentProviders::Null.new
-  end
-
-  def current_provider_position
-    @current_provider_position ||= 0
-  end
-
-  def has_more_providers?
-    current_provider_position + 1 < providers.count
+  def provider_exists?(position)
+    providers.exists?(position: position)
   end
 end
--- spec/features/activity_tracker_spec.rb
+++ spec/features/activity_tracker_spec.rb
@@ -6,7 +6,7 @@ describe 'activity tracker' do
   describe 'with a slideshow' do
     it 'should allow slideshow navigation' do
       sign_in_participant participants(:participant1)
-      visit '/activity_tracker'
+      visit '/navigator/contexts/activity_tracker'
       find('.next-button').click
 
       expect(page).to have_text(slides(:slide2).body)
