b1b82e2ec8e32cc3970ca0669b7e639efe534159
Michael Wehrley <michael.wehrley@gmail.com>
2014-02-25 10:19:35 -0600

    fixed context

--- app/models/content_providers/activity_form.rb
+++ app/models/content_providers/activity_form.rb
@@ -1,8 +1,10 @@
 class ContentProviders::ActivityForm < ContentProvider
 
   def render_current(view_context)
-    view_context.render(template: 'activity/new', locals: {
-        hours: [7,8,9,10,11,12,13,14,15,16,17],
+    view_context.render(
+      template: 'activity/new',
+      locals: {
+        hours: [7,8,9,10,11,12,13,14,15,16,17], #this will be dynamic...
         activity: Activity.new,
         create_path: view_context.participant_data_path
       }
