34fedfd6593898c9e9a85be5a4e565f0ccad8165
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-02-25 14:43:06 -0600

    added pleasurable activity index provider

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,6 @@
 class Activity < ActiveRecord::Base
+  PLEASURABLE_CUTOFF = 6
+
   belongs_to :activity_type
   belongs_to :participant
 
@@ -6,6 +8,10 @@ class Activity < ActiveRecord::Base
   validates_inclusion_of :is_complete, in: [true, false]
 
   delegate :title, to: :activity_type, prefix: false
+
+  scope :pleasurable, -> do
+    where('activities.actual_pleasure_intensity >= ?', PLEASURABLE_CUTOFF)
+  end
 
   def self.during(start_time, end_time)
     where('start_time >= ? AND end_time <= ?', start_time, end_time)
