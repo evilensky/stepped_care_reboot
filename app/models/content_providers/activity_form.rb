5e0582793b037235ad0cfcf64f521cf600178ba6
Michael Wehrley <michael.wehrley@gmail.com>
2014-02-25 14:27:25 -0600

    nearly finished timezone form

--- app/models/content_providers/activity_form.rb
+++ app/models/content_providers/activity_form.rb
@@ -1,10 +1,19 @@
 class ContentProviders::ActivityForm < ContentProvider
 
+  require "#{Rails.root}/lib/time"  
+  
   def render_current(view_context, position)
+    most_recent_awake_period = AwakePeriod.last
+    start_time = most_recent_awake_period.start_time
+    end_time = most_recent_awake_period.end_time
+    timestamps = []
+    (start_time.to_i .. (end_time.to_i - 1.hour)).step(1.hour) do |timestamp|
+      timestamps << timestamp
+    end
     view_context.render(
       template: 'activity/new',
       locals: {
-        hours: [7,8,9,10,11,12,13,14,15,16,17], #this will be dynamic...
+        timestamps: timestamps,
         activity: Activity.new,
         create_path: view_context.participant_data_path
       }
