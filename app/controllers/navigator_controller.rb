1d7711db98309e6e974a9b446cfffe3c746a6934
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-02-24 21:59:01 -0600

    split authentication between users and participants; tune nav query

--- app/controllers/navigator_controller.rb
+++ app/controllers/navigator_controller.rb
@@ -1,4 +1,6 @@
 class NavigatorController < ApplicationController
+  before_filter :authenticate_participant!
+
   def show_context
     @navigator = Navigator.new(session)
     @navigator.initialize_context(params[:context_name] || 'home')
