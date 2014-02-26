721868a7a3de7bef94e2f3a34c45d3beb398caa2
Tag0Mag0 <jem.hilton@gmail.com>
2014-02-26 16:46:26 -0600

    added random activity checklist content provider

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -8,7 +8,7 @@ class Activity < ActiveRecord::Base
   belongs_to :activity_type
   belongs_to :participant
 
-  validates_presence_of :activity_type, :activity_type_title, :participant, :start_time, :end_time
+  validates_presence_of :activity_type, :participant
   validates_inclusion_of :is_complete, in: [true, false]
 
   delegate :title, to: :activity_type, prefix: false, allow_nil: true
@@ -21,6 +21,10 @@ class Activity < ActiveRecord::Base
 
   scope :accomplished, -> do
     where('activities.actual_accomplishment_intensity >= ?', ACCOMPLISHED_CUTOFF)
+  end
+
+  scope :random, -> do
+    order("RANDOM()")
   end
 
   def self.during(start_time, end_time)
