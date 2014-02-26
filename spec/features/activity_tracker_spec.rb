7fea5fc57d2dcb59abe95f58cf909bea2ef6b9bf
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-02-26 10:23:04 -0600

    fix broken test

--- spec/features/activity_tracker_spec.rb
+++ spec/features/activity_tracker_spec.rb
@@ -3,23 +3,24 @@ require 'spec_helper'
 describe 'activity tracker' do
   fixtures :participants, :slideshows, :slides, :content_modules, :content_providers
 
-  describe 'with a module and multiple providers' do
-    it 'should list the module on the landing page' do
+  describe 'with multiple modules and multiple providers' do
+    before do
       sign_in_participant participants(:participant1)
       visit '/navigator/contexts/activity_tracker'
+    end
 
+    it 'should list the modules on the landing page' do
       expect(page).to have_text(content_modules(:activity_tracker_module2).title)
     end
 
-    it 'should allow navigation within the slideshow' do
-      sign_in_participant participants(:participant1)
-      visit '/navigator/contexts/activity_tracker'
+    it 'should allow navigation through the modules' do
       click_on content_modules(:activity_tracker_module2).title
+
+      expect(page).to have_text(slides(:do_awareness_intro1).body)
+
       find('.next-button').click
 
-      expect(page).to have_text(slides(:slide2).body)
-
-      find('.next-button').click
+      expect(page).to have_text("OK, let's talk about yesterday.")
     end
   end
 end
