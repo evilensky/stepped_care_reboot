45d3912b0ae3bdf5dc77758e6703179eb9a80136
Tag0Mag0 <jem.hilton@gmail.com>
2014-02-27 13:11:08 -0600

    created new content provider for unplanned activities form

---
+++ app/models/content_providers/unplanned_activity_form.rb
@@ -0,0 +1,33 @@
+class ContentProviders::UnplannedActivityForm < ContentProvider
+
+  def render_current(options)
+    options.view_context.render(
+      template: 'activities/unplanned_activity_form',
+      locals: {
+        activities: options.participant.activities.random.unplanned.first(4),
+        create_path: options.view_context.participant_data_path
+      }
+    )
+  end
+
+  def data_attributes
+    [:start_time, :end_time, :activity_type_title, :actual_pleasure_intensity, :actual_accomplishment_intensity]
+  end
+
+  def exists?(position)
+    false
+  end
+
+  def data_class_name
+    'Activity'
+  end
+
+  def has_more_content?
+    false
+  end
+
+  def show_nav_link?
+    false
+  end
+
+end
