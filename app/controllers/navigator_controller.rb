a670570e8f183680417eb1123df426165facf0cb
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-02-27 07:58:08 -0600

    added more content to fixtures; enabled direct links to modules

--- app/controllers/navigator_controller.rb
+++ app/controllers/navigator_controller.rb
@@ -1,15 +1,30 @@
 class NavigatorController < ApplicationController
-  before_filter :authenticate_participant!
+  before_action :authenticate_participant!, :instantiate_navigator
 
   def show_context
-    @navigator = Navigator.new(session)
     @navigator.initialize_context(params[:context_name] || 'home')
     render 'show_content'
   end
 
+  def show_module
+    begin
+      @navigator.initialize_module(params[:module_id])
+    rescue ActiveRecord::RecordNotFound
+      @navigator.initialize_context('home')
+      flash[:alert] = 'Unable to find that module.'
+    ensure
+      render 'show_content'
+    end
+  end
+
   def show_next_content
-    @navigator = Navigator.new(session)
     @navigator.fetch_next_content
     render 'show_content'
+  end
+
+  private
+
+  def instantiate_navigator
+    @navigator = Navigator.new(session)
   end
 end
