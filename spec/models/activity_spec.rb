5346caf21aba4a814880796722da19232f9b4822
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-02-28 11:23:54 -0600

    refactored activity spec

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -4,15 +4,16 @@ describe Activity do
 	fixtures [:participants, :activity_types, :activities]
 
 	describe "create activity type" do
+    let(:title) { 'prancing in the woods' }
 
 		it "should create an activity type when it has an activity type title" do
 			activity = Activity.create(
         participant: participants(:participant1),
-        activity_type_title: "prancing in the woods",
+        activity_type_title: title,
         start_time: Time.now,
         end_time: Time.now + 1.hour
       )
-			expect(participants(:participant1).activity_types.exists?(title: "prancing in the woods")).to be true
+			expect(participants(:participant1).activity_types.exists?(title: title)).to be true
 		end
 
 		it "should not create an activity type when it does not have an activity type title" do
@@ -21,22 +22,18 @@ describe Activity do
         start_time: Time.now,
         end_time: Time.now + 1.hour
       )
-			expect(participants(:participant1).activity_types.exists?(title: "prancing in the woods")).to be false
+			expect(participants(:participant1).activity_types.exists?(title: title)).to be false
 		end
-
 	end
 
 	describe "scopes" do
-
 		it "unplanned should only show activities where start_time and end_time are nil" do
 			unplanned_activities = Activity.unplanned
 			expect(unplanned_activities.all).not_to be_empty
 			unplanned_activities.each do |a|
-				a.start_time.should be_nil
-				a.end_time.should be_nil
+				expect(a.start_time).to be_nil
+				expect(a.end_time).to be_nil
 			end
 		end
-
 	end
-
 end
