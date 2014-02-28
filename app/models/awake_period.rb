1609b0c6286beaa900da5d63c545095c9b656680
Tag0Mag0 <jem.hilton@gmail.com>
2014-02-28 10:23:58 -0600

    end of modules now returns back to tool landing module

--- app/models/awake_period.rb
+++ app/models/awake_period.rb
@@ -3,6 +3,7 @@ class AwakePeriod < ActiveRecord::Base
 
   validates_presence_of :participant, :start_time, :end_time
   validate :end_time_after_start_time
+  # validate :awake_periods_cannot_overlap
 
   def end_time_after_start_time
     if start_time && end_time && end_time <= start_time
--- app/models/navigator.rb
+++ app/models/navigator.rb
@@ -33,9 +33,8 @@ class Navigator
       @state[:content_position] = 1
       @state[:provider_position] += 1
     elsif ContentModule.exists?(context: @state[:context], position: @state[:module_position] + 1)
-      next_module_position = @state[:module_position] + 1
       initialize_context(@state[:context])
-      @state[:module_position] = next_module_position
+      @state[:module_position] = 1
     else
       initialize_context(@state[:context])
     end
