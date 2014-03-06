74718d8bc095e9e989f611fd78495f5ea19605d3
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-03-06 08:39:54 -0600

    various stylesheet and slideshow ui fixes

--- app/assets/javascripts/slideshows.js
+++ app/assets/javascripts/slideshows.js
@@ -1,12 +1,14 @@
-sort = function() {
-  return $('#sortable-slides').sortable({
-    axis: 'y',
-    handle: '.handle',
-    update: function() {
-      return $.post($(this).data('update-url'), $(this).sortable('serialize'));
-    }
-  });  
-};
+(function() {
+  function sort() {
+    $('#sortable-slides').sortable({
+      axis: 'y',
+      handle: '.handle',
+      update: function() {
+        $.post($(this).data('update-url'), $(this).sortable('serialize'));
+      }
+    });
+  };
 
-$(document).ready(sort)
-$(document).on('page:load', sort)
+  $(document).ready(sort);
+  $(document).on('page:load', sort);
+})();
