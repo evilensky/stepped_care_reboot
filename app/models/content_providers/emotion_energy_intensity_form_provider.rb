189799cd35a38739509e11e62410683d00938e9f
michaelwehrley <michaelwehrley@gmail.com>
2014-03-11 16:58:36 -0500

    updated rating for mapping

--- app/models/content_providers/emotion_energy_intensity_form_provider.rb
+++ app/models/content_providers/emotion_energy_intensity_form_provider.rb
@@ -3,20 +3,20 @@ class ContentProviders::EmotionEnergyIntensityFormProvider < BitPlayer::ContentP
     emotion = options.view_context.current_participant.emotions.last
 
     case emotion.rating
-    when 1..2
-      emotion_types = [["Alert", 4], ["Excited", 3], ["Elated", 2], ["Happy", 1]]
-    when 3..5
-      emotion_types = [["Contented", -1], ["Serene", -2], ["Relaxed", -3], ["Calm", -4]]
-    when 5
-      emotion_types = [["Relaxed", -3], ["Calm", -4], ["Tense", 4], ["Nervous", 3]]
-    when 6..8
-      emotion_types = [["Tense", 4], ["Nervous", 3], ["Stressed", 2], ["Upset", 1]]
-    when 9..10
-      emotion_types = [["Sad", -1], ["Depressed", -2], ["Bored", -3], ["Fatigued", -4]]
+    when -5..-4
+      emotion_types = [["Depressed", -2], ["Sad", -1], ["Stressed", 2], ["Upset", 1]]
+    when -3..-2
+      emotion_types = [["Bored", -3], ["Fatigued", -4], ["Nervous", 3], ["Tense", 4]]
+    when -1..1
+      emotion_types = [["Alert", 4], ["Calm", -4], ["Fatigued", -4], ["Tense", 4]]
+    when 2..3
+      emotion_types = [["Alert", 4], ["Calm", -4], ["Excited", 3], ["Relaxed", -3]]
+    when 4..5
+      emotion_types = [["Contented", -1], ["Elated", 2], ["Happy", 1], ["Serene", -2]]
     else
       emotion_types = []
     end
-    
+
     options.view_context.render(
       template: 'emotions/energy_intensity',
       locals: {
