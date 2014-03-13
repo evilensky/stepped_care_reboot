c1afb4219109c8d4577bf683f72150c06b95cbb9
michaelwehrley <michaelwehrley@gmail.com>
2014-03-13 12:16:27 -0500

    updated views to match mockups

--- app/models/content_providers/emotion_energy_intensity_form_provider.rb
+++ app/models/content_providers/emotion_energy_intensity_form_provider.rb
@@ -1,27 +1,10 @@
 class ContentProviders::EmotionEnergyIntensityFormProvider < BitPlayer::ContentProvider
   def render_current(options)
-    emotion = options.view_context.current_participant.emotions.last
-
-    case emotion.rating
-    when -5..-4
-      emotion_types = [["Depressed", -2], ["Sad", -1], ["Stressed", 2], ["Upset", 1]]
-    when -3..-2
-      emotion_types = [["Bored", -3], ["Fatigued", -4], ["Nervous", 3], ["Tense", 4]]
-    when -1..1
-      emotion_types = [["Alert", 4], ["Calm", -4], ["Fatigued", -4], ["Tense", 4]]
-    when 2..3
-      emotion_types = [["Alert", 4], ["Calm", -4], ["Excited", 3], ["Relaxed", -3]]
-    when 4..5
-      emotion_types = [["Contented", -1], ["Elated", 2], ["Happy", 1], ["Serene", -2]]
-    else
-      emotion_types = []
-    end
-
     options.view_context.render(
-      template: 'emotions/energy_intensity',
+      template: 'emotions/edit',
       locals: {
-        emotion: emotion,
-        emotion_types: emotion_types,
+        emotion: options.view_context.current_participant.emotions.last,
+        emotion_types: ["Alert", "Calm"],
         update_path: options.view_context.participant_data_path
       }
     )
