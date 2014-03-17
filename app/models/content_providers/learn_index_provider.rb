ffbc49a7789374b64c49f043421ea55619184400
michaelwehrley <michaelwehrley@gmail.com>
2014-03-17 16:18:08 -0500

    started working on lessons with modules displaying

---
+++ app/models/content_providers/learn_index_provider.rb
@@ -0,0 +1,11 @@
+# module ContentProviders
+#   # Provides a view of current learning tools: videos and lessons
+#   class LearnIndexProvider < BitPlayer::ContentProvider
+
+#     def render_current(options)
+#       options.view_context.render(
+#       )
+#     end
+
+#   end
+# end
