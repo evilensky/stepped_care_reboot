f9cbcb8b7b90c418d9d057b4f40af55f7c0ea85c
michaelwehrley <michaelwehrley@gmail.com>
2014-03-18 10:58:25 -0500

    finished structure for participants to see all lessons available

---
+++ app/models/content_providers/learn_lessons_index_provider.rb
@@ -0,0 +1,20 @@
+module ContentProviders
+  # Provides a view of current learning tools: videos and lessons
+  class LearnLessonsIndexProvider < BitPlayer::ContentProvider
+
+    def render_current(options)
+
+      content_modules = BitPlayer::ContentModule
+
+      options.view_context.render(
+        template: 'learn/lessons_index',
+        locals: { content_modules: content_modules.all }
+      )
+    end
+
+    def show_nav_link?
+      false
+    end
+
+  end
+end
