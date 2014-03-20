6fad943fa16b2f4d3eee112307656add2228c0b7
michaelwehrley <michaelwehrley@gmail.com>
2014-03-20 12:59:58 -0500

    updated unassign slideshow spec

--- spec/features/learn_spec.rb
+++ spec/features/learn_spec.rb
@@ -11,12 +11,15 @@ describe 'learning for participants' do
     visit '/navigator/contexts/library'
   end
 
-  it 'should allow a participant to select what to learn' do
+  it 'will be able to select what to learn' do
     expect(page).to have_link('Lessons')
     expect(page).to have_link('Videos')
   end
 
-  it 'should be able to view assigned slideshow'
+  it 'will be able to view assigned slideshow' do
+    click_on "Lessons"
+    save_and_open_page
+  end
 
 end
 
@@ -31,7 +34,7 @@ describe 'learning for users' do
     sign_in_user users(:user1)
   end
 
-  it 'should be able to assign slideshow to group' do
+  it 'will be able to assign slideshow to group' do
     group = groups(:group1)
     bit_player_slideshow = bit_player_slideshows(:rspec_slideshow)
     visit slideshow_path(bit_player_slideshow)
@@ -43,7 +46,7 @@ describe 'learning for users' do
     select('Group1', from: "Select Group")
     fill_in('Select number of days into intervention to make available', with: 1)
     click_on('Save')
-    current_path.should eq slideshow_path(bit_player_slideshow)
+    expect(current_path).to eq slideshow_path(bit_player_slideshow)
     expect(gsj.first).not_to be_nil
     expect(gsj.first.group).to eq group
     expect(gsj.first.release_day).to eq 1
@@ -52,7 +55,8 @@ describe 'learning for users' do
     expect(page).to have_text(1)
   end
 
-  it 'should be able to unassign slideshow to group' do
+  it 'will be able to update slideshow to group' do
+    # I couldn't get group_slideshow_joins.yml to create the correct group_slideshow_join -Wehrley 3/20/14
     user = users(:user1)
     group1 = groups(:group1)
     group2 = groups(:group2)
@@ -75,10 +79,41 @@ describe 'learning for users' do
     expect(gsj1.release_day).to eq 2
   end
 
-  it 'should be able to update slideshow to group'
+  it "validation that the same slideshow can't be assigned more than once" do
+    # I couldn't get group_slideshow_joins.yml to create the correct group_slideshow_join -Wehrley 3/20/14
+    user = users(:user1)
+    group = groups(:group1)
+    bit_player_slideshow = bit_player_slideshows(:rspec_slideshow2)
+    GroupSlideshowJoin.create(
+      bit_player_slideshow_id: bit_player_slideshow.id,
+      creator_id: user.id,
+      group_id: group.id)
+    visit slideshow_path(bit_player_slideshow)
+    click_on "Assign"
+    select('Group1', from: "Select Group")
+    fill_in('Select number of days into intervention to make available', with: 1)
+    click_on('Save')
+    expect(page).to have_text("Unable to assign slideshow")
+    expect(page).to have_text("Release day This slideshow has already been assigned and set to be released on this day to this group.")
+  end
 
-  it "validation that the same slideshow can't be assigned more than once"
-
-  it 'should be able to see groups slideshow is assigned to'
+  it 'will be able to unassign slideshow to group' do
+    # I couldn't get group_slideshow_joins.yml to create the correct group_slideshow_join -Wehrley 3/20/14
+    user = users(:user1)
+    group1 = groups(:group1)
+    group2 = groups(:group2)
+    bit_player_slideshow = bit_player_slideshows(:rspec_slideshow2)
+    gsj = GroupSlideshowJoin.create(
+      bit_player_slideshow_id: bit_player_slideshow.id,
+      creator_id: user.id,
+      group_id: group1.id)
+    gsj_id = gsj.id
+    expect(GroupSlideshowJoin.where(id: gsj_id).count).not_to eq 0
+    visit slideshow_path(bit_player_slideshow)
+    with_scope "#group-slide-show-#{gsj.id}" do
+      click_on "Remove"
+    end
+    expect(GroupSlideshowJoin.where(id: gsj_id).count).to eq 0
+  end
 
 end
