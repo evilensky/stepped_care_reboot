ce93a1179345aef8ca097eb2bb762535d9148ec7
michaelwehrley <michaelwehrley@gmail.com>
2014-03-24 11:27:24 -0500

    removed 'Videos' link

--- spec/features/learn_spec.rb
+++ spec/features/learn_spec.rb
@@ -14,7 +14,7 @@ describe 'participant1 learns via slideshows' do
 
   it 'can select what to learn' do
     expect(page).to have_link('Lessons')
-    expect(page).to have_link('Videos')
+    # expect(page).to have_link('Videos')
   end
 
   it 'can view assigned slideshow that are released' do
@@ -24,18 +24,17 @@ describe 'participant1 learns via slideshows' do
     expect(page).not_to have_link('This slide is ONLY AVAILABLE ON DAY 3!')
   end
 
-  it 'can view videos' do
-    # I'm not sure yet what it is exactly we want to display...slides? slideshows?
-    click_on "Videos"
-    expect(page).to have_link('Slide 4') # Giraffe & Hippo video
-    click_on "Slide 4"
-    video_slide = BitPlayer::VideoSlide.find_by_title('Slide 4')
-    expect(current_path).to eq navigator_location_path(
-      module_id: video_slide.slideshow.content_provider.content_module.id,
-      provider_id: video_slide.slideshow.content_provider.id,
-      content_position: video_slide.position
-    )
-  end
+  it 'can view videos'# do
+  #   click_on "Videos"
+  #   expect(page).to have_link('Slide 4') # Giraffe & Hippo video
+  #   click_on "Slide 4"
+  #   video_slide = BitPlayer::VideoSlide.find_by_title('Slide 4')
+  #   expect(current_path).to eq navigator_location_path(
+  #     module_id: video_slide.slideshow.content_provider.content_module.id,
+  #     provider_id: video_slide.slideshow.content_provider.id,
+  #     content_position: video_slide.position
+  #   )
+  # end
 
 end
 
