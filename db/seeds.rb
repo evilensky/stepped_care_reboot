7c6d57bb9d63a0a056d65cb653d751e73869b936
michaelwehrley <michaelwehrley@gmail.com>
2014-03-24 17:08:03 -0500

    added csv and seed file so we can input all slides

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.transaction do
+  content_module = BitPlayer::ContentModule.find_by_title("Lessons")
+  ["slideshows.csv"].each do |file|
+    CSV.foreach(File.join(Rails.root, "db", file), headers: true) do |row|
+      ss = BitPlayer::Slideshow.new(title: row[1])
+      if ss.save
+        content_provider = BitPlayer::ContentProvider.new(
+          type: BitPlayer::ContentProviders::SlideshowProvider.to_s,
+          source_content_type: "BitPlayer::Slideshow",
+          source_content_id: ss.id,
+          bit_player_content_module_id: content_module.id,
+          position: row[0].to_i
+        )
+        unless content_provider.save
+          fail "There were errors: #{content_provider.errors.full_messages}"
+        end
+        puts "Position: #{content_provider.position}"
+      else
+        fail "There were errors: #{ss.errors.full_messages}"
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.transaction do
+  ["slides.csv"].each do |file|
+    CSV.foreach(File.join(Rails.root, "db", file), headers: true) do |row|
+      s = BitPlayer::Slide.new(
+        title: row[2] || "Untitled",
+        body: row[3],
+        position: row[1].to_i,
+        bit_player_slideshow_id: BitPlayer::Slideshow.find_by_title(row[0]).id,
+        is_title_visible: !row[2].nil?
+      )
+      unless s.save
+        fail "There were errors: #{s.errors.full_messages}"
+      end
+      puts "Title #{s.title}"
+    end
+  end
+end
+
+# end
