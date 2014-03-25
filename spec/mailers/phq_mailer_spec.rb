8ff45fc30672eab97e73a147e45252627923aeb7
Eric Schlange <eric.schlange@northwestern.edu>
2014-03-25 10:47:28 -0500

    Added test case for new mailer.

--- spec/mailers/phq_mailer_spec.rb
+++ spec/mailers/phq_mailer_spec.rb
@@ -1,5 +1,16 @@
 require "spec_helper"
 
 describe PhqMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :participants
+
+  describe "reminder_email" do
+    let(:mail) { PhqMailer.reminder_email(participants(:participant1)) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq('PHQ-9 Reminder')
+      expect(mail.to).to eq([participants(:participant1).email])
+      expect(mail.from).to eq(['stepcare@northwestern.edu'])
+    end
+
+  end
 end
