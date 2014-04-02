f4904c202c500f3eeffc47ed42f291f55ec4e738
michaelwehrley <michaelwehrley@gmail.com>
2014-04-02 09:59:01 -0500

    tests passing

--- spec/features/learn_spec.rb
+++ spec/features/learn_spec.rb
@@ -73,46 +73,53 @@ describe "users CRUDing learning by assigning learning slideshows to groups" do
   it "can assign slideshow to group" do
     group = groups(:group1)
     bit_player_slideshow = bit_player_slideshows(:rspec_slideshow)
-    visit slideshow_path(bit_player_slideshow)
-    expect(page).not_to have_text(group.title)
-    expect(page).not_to have_text(1)
-    gsj = GroupSlideshowJoin.where(group_id: group.id, bit_player_slideshow_id: bit_player_slideshow.id)
-    expect(gsj.first).to be_nil
+    visit edit_group_path(group)
+    with_scope "table" do
+      expect(page).not_to have_text(bit_player_slideshow.title)
+      expect(page).not_to have_text(10)
+    end
+    gsj = GroupSlideshowJoin.where(group_id: group.id, bit_player_slideshow_id: bit_player_slideshow.id, release_day: 10).first
+    expect(gsj).to be_nil
+    select("I'm a test!", from: "Select Slideshow")
+    fill_in("Select number of days into intervention to make available", with: 10)
     click_on "Assign"
-    select("Group 1", from: "Select Group")
-    fill_in("Select number of days into intervention to make available", with: 1)
-    click_on("Save")
-    expect(current_path).to eq slideshow_path(bit_player_slideshow)
-    expect(gsj.first).not_to be_nil
-    expect(gsj.first.group).to eq group
-    expect(gsj.first.release_day).to eq 1
-    expect(gsj.first.creator).to eq users(:user1)
-    expect(page).to have_text(group.title)
-    expect(page).to have_text(1)
+    expect(current_path).to eq edit_group_path(group)
+    gsj = GroupSlideshowJoin.where(group_id: group.id, bit_player_slideshow_id: bit_player_slideshow.id, release_day: 10).first
+    expect(gsj).not_to be_nil
+    expect(gsj.group).to eq group
+    expect(gsj.bit_player_slideshow).to eq bit_player_slideshow
+    expect(gsj.release_day).to eq 10
+    expect(gsj.creator).to eq users(:user1)
+    with_scope "table" do
+      expect(page).to have_text(bit_player_slideshow.title)
+      expect(page).to have_text(10)
+    end
   end
 
-  it "can update slideshow to group" do
+  it "can update the slideshow and date assigned to a group" do
     gsj = group_slideshow_joins(:group_slideshow_join1)
-    expect(gsj.group_id).to eq groups(:group1).id
+    expect(gsj.bit_player_slideshow_id).to eq bit_player_slideshows(:rspec_slideshow2).id
     expect(gsj.release_day).to eq 1
-    visit slideshow_path(gsj.bit_player_slideshow)
-    with_scope "#group-slide-show-#{gsj.id}" do
+    visit edit_group_path(gsj.group)
+    with_scope "#group-slideshow-join-#{gsj.id}" do
       click_on "Edit"
     end
-    select("Group 2", from: "Select Group")
-    fill_in("Select number of days into intervention to make available", with: 2)
+    select("Happiness", from: "Select Slideshow")
+    fill_in("Select number of days into intervention to make available", with: 3)
     click_on "Update"
     gsj.reload
-    expect(gsj.group_id).to eq groups(:group2).id
-    expect(gsj.release_day).to eq 2
+    expect(gsj.bit_player_slideshow_id).to eq bit_player_slideshows(:rspec_slideshow3).id
+    expect(gsj.release_day).to eq 3
   end
 
   it "validation that the same slideshow can't be assigned more than once" do
-    visit slideshow_path(group_slideshow_joins(:group_slideshow_join1).bit_player_slideshow)
-    click_on "Assign"
-    select("Group 1", from: "Select Group")
+    gsj = group_slideshow_joins(:group_slideshow_join1)
+    expect(gsj.bit_player_slideshow_id).to eq bit_player_slideshows(:rspec_slideshow2).id
+    expect(gsj.release_day).to eq 1
+    visit edit_group_path(gsj.group)
+    select("#{gsj.bit_player_slideshow.title}", from: "Select Slideshow")
     fill_in("Select number of days into intervention to make available", with: 1)
-    click_on("Save")
+    click_on "Assign"
     expect(page).to have_text("Unable to assign slideshow")
     expect(page).to have_text("Release day This slideshow has already been assigned and set to be released on this day to this group.")
   end
@@ -120,8 +127,8 @@ describe "users CRUDing learning by assigning learning slideshows to groups" do
   it "can unassign slideshow to group" do
     gsj = group_slideshow_joins(:group_slideshow_join1)
     expect(GroupSlideshowJoin.where(id: gsj.id).count).not_to eq 0
-    visit slideshow_path(gsj.bit_player_slideshow)
-    with_scope "#group-slide-show-#{gsj.id}" do
+    visit edit_group_path(gsj.group)
+    with_scope "#group-slideshow-join-#{gsj.id}" do
       click_on "Remove"
     end
     expect(GroupSlideshowJoin.where(id: gsj.id).count).to eq 0
