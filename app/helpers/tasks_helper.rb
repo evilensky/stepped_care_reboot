c0aaf22ec68fb0c9109c13ae2ba6b27bd7f0833f
michaelwehrley <michaelwehrley@gmail.com>
2014-04-08 12:01:19 -0500

    asterisk now shows for tools and is tested

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,12 +1,15 @@
 # Used to display asterisks if tasks and tools have been assigned to a group
 module TasksHelper
-  def assign_tool(participant, tool_name)
-    task_
-    # context_array = []
-    # Task.to_be_completed_by_participant(participant).each do |task|
-    #   context_array << task.bit_player_content_module.context
-    # end
-    # context_array.include? tool_name
+
+  def assign_tool(context, section)
+    if context == section.slug
+      text = section.label + " " + fa_icon('caret-right')
+    elsif context != section.slug and TaskStatus.to_complete(current_participant, BitPlayer::ContentModule.where(context: section.slug)).count > 0
+      text = fa_icon('asterisk') + " " + section.label
+    else
+      text = section.label
+    end
+    link_to text.html_safe, navigator_context_path(context_name: section.slug)
   end
 
   def content_module_link(content_module, tasks_to_be_completed)
@@ -15,9 +18,9 @@ module TasksHelper
       title = content_module.title
       task_id = false
     else
-      title = fa_icon('asterisk') + content_module.title
+      title = fa_icon('asterisk') + " " + content_module.title
       task_id = tasks_for_content_module.first.id
     end
-    link_to title, navigator_location_path(module_id: content_module.id), data: { task_status_id: task_id }, class: "module"
+    link_to title.html_safe, navigator_location_path(module_id: content_module.id), data: { task_status_id: task_id }, class: "module"
   end
 end
